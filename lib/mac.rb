9bed65047831d335f2fcbda4993ec73d64372007
Jordan Byron <jordan.byron@gmail.com>
2012-08-10 16:20:52 -0400

    Add new photos to the site and re-enable scores

--- lib/mac.rb
+++ lib/mac.rb
@@ -8,19 +8,19 @@ module Mac
   def events
     events = []
     agent = Mechanize.new
-        page  = agent.get('http://musicalartsconference.com/members/Guard/Naugatuck-HS-Winter-Guard.html')
-        page.search("div[@class='about_page'] table tr")[1..-1].each do |row|
-          name_row = row.search("td a")
-          name     = name_row.inner_html
-          url      = "http://musicalartsconference.com#{name_row.first.attributes["href"].value}"
-          tds = row.search("td").map {|td| td.inner_html }
+    page  = agent.get('http://musicalartsconference.com/members/Marching-Band/Naugatuck-High-School.html')
 
-          next if name[/Beecher/i] # Skip Beecher
+    page.search("div[@class='about_page'] table tr")[1..-1].each do |row|
+      name_row = row.search("td a")
+      next unless name_row
+      name     = name_row.inner_html
+      url      = "http://musicalartsconference.com#{name_row.first.attributes["href"].value}"
+      tds = row.search("td").map {|td| td.inner_html }
 
-          event = Event.new(name, url, tds[1], tds[2])
+      event = Event.new(name, url, tds[1], tds[2])
 
-          events << event
-        end
+      events << event
+    end
 
     events
   end
