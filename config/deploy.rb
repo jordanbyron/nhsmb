c989c5b64d314e5356f2662675b77b77bfefaeeb
Jordan Byron <jordan.byron@gmail.com>
2012-08-19 15:06:39 -0400

    DRY up deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,13 +17,11 @@ after 'deploy:update_code' do
   remote_path = shared_path[2..-1]
 
   run "cd #{release_path}; mm-build"
-  run "ln -nfs #{shared_path}/downloads/audio #{release_path}/build/downloads/"
-  run "ln -nfs #{shared_path}/downloads/documents #{release_path}/build/downloads/"
-  run "ln -nfs #{shared_path}/downloads/sheet_music #{release_path}/build/downloads/"
 
-  run_locally "rsync -ruv public/downloads/documents/* jordanbyron@jordanbyron.com:#{remote_path}/downloads/documents/"
-  run_locally "rsync -ruv public/downloads/audio/* jordanbyron@jordanbyron.com:#{remote_path}/downloads/audio/"
-  run_locally "rsync -ruv public/downloads/sheet_music/* jordanbyron@jordanbyron.com:#{remote_path}/downloads/sheet_music/"
+  %w{audio documents sheet_music drill}.each do |folder|
+    run "ln -nfs #{shared_path}/downloads/#{folder} #{release_path}/build/downloads/"
+    run_locally "rsync -ruv public/downloads/#{folder}/* jordanbyron@jordanbyron.com:#{remote_path}/downloads/#{folder}/"
+  end
 end
 
 after "deploy", "deploy:cleanup"
