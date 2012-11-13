b5a3abeb75894b68df9ac3aaadb64f805d8cafc9
Jordan Byron <jordan.byron@gmail.com>
2012-11-13 12:27:01 -0500

    Disable auto update and write scores to flat file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 set :output, "/home/jordanbyron/nhsmb/logs/cron_log.log"
 
-every 1.day do
-  command 'cd /home/jordanbyron/nhsmb/current; mm-build'
-end
+# Disable until the Spring
+# every 1.day do
+#   command 'cd /home/jordanbyron/nhsmb/current; mm-build'
+# end
