875b25c75865441832812784c3c327f77914f47a
Jordan Byron <jordan.byron@gmail.com>
2013-09-27 18:32:47 -0400

    Enable bitches

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,5 @@
 set :output, "/home/jordanbyron/nhsmb/logs/cron_log.log"
 
-# Disable until the Spring
-# every 1.day do
-#   command 'cd /home/jordanbyron/nhsmb/current; mm-build'
-# end
+every 1.day do
+  command 'cd /home/jordanbyron/nhsmb/current; mm-build'
+end
