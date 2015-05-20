cc18f31cdf6af4e2a86934d6200ce86cf6efec03
Jordan Byron <jordan.byron@gmail.com>
2015-05-20 14:41:52 -0400

    Disable scores page

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "/home/jordanbyron/nhsmb/logs/cron_log.log"
 
-every 1.day do
-  command 'cd /home/jordanbyron/nhsmb/current; mm-build'
-end
+# every 1.day do
+#   command 'cd /home/jordanbyron/nhsmb/current; mm-build'
+# end
